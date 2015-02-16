905b5a89f35c20cfdb120aa8caef5c22c3dca572
Natalie Silmon de Monerri <nsilmondemonerri@gmail.com>
2015-02-16 10:39:06 -0500

    cachematrix.R
    R script that takes a square or complex matrix, caches its inverse, and
    returns the inverse of the input matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This script will take a square or complex matrix, cache its inverse, and return the inverse of the input matrix.
 
-## Write a short comment describing this function
+#makeCacheMatrix creates a special matrix object that can cache its inverse. #Note: Non-square and non-complex matrices cannot have an inverse!
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<-function(x=matrix()){
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-function()x
+	 setmatrix <- function(solve) m <<- solve
+            getmatrix <- function() m
+            list(set = set, get = get,
+                 setmatrix = setmatrix,
+                 getmatrix = getmatrix)
+	}
 
-}
-
-
-## Write a short comment describing this function
+#cacheSolve function takes a cacheable matrix as input and returns the inverse of that matrix.
 
 cacheSolve <- function(x, ...) {
+		    m<-x$getmatrix()
+	            if(!is.null(m)) {
+                    message("getting cached data")
+                    return(m)
+            }
+            data <- x$get()
+            m <- solve(data, ...)
+            x$setmatrix(m)
+            m
+
+	
         ## Return a matrix that is the inverse of 'x'
 }
